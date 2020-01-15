9816224657f5e4a79ada124ebb50b9c98ad535ae
usw344 <22089726+usw344@users.noreply.github.com>
2020-01-15 00:03:03 -0600

    intizaltion of repo

---
+++ a1q1.py
@@ -0,0 +1,34 @@
+def check_diagonals(square):
+    '''
+    param square: a 3d list containing 9 ints
+    return: True if all the diagonals add up to 15. False otherwise
+    '''
+def check_columns(square):
+    '''
+    param square: a 3d list containing 9 ints
+    return: True if all the columns sum to 15. False otherwiseo
+    '''
+def check_rows(square):
+    '''
+    param square: a 3d list containing 9 ints
+    return: True if all the rows sum to 15. False otherwise
+    '''
+def check_range(square):
+    '''
+    param square: a 3d list containing 9 ints
+    return: True if the square contains all the integers 1 .. 9. False otherwise
+    '''
+
+### Use these 3 functions
+def check_square(square):
+    '''
+    param square: a 3d list containing 9 ints
+    return:True if the square has all the properties of a magic square; False otherwise 
+    '''
+def get_square():
+    '''
+    return: a 3x3 list of lists
+    '''
+def main():
+    '''
+    '''
